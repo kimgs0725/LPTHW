9bb01e5a84cb9149769243a8afcc71b4263d26b5
김경수 <kgs@kgs-MacBook-Pro.local>
2019-05-08 00:14:22 +0900

    ex46, ex47 커밋 추가

---
+++ ex47/skeleton/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from disutils.core import setup
+
+config = [
+    'description': 'a tiny project',
+    'author': 'gyeongsoo_kim',
+    'url': 'url to get it at.',
+    'download_url': 'where to download it.',
+    'author_email': 'kimgs0725@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex47'],
+    'scripts': [],
+    'name': 'a little bit game'
+]
+setup(**config)
